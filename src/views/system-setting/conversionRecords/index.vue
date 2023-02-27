e9226e67764ff18af52d1fcb1e7ac00f402416e2
scalarvector1003 <happysmile911003@gmail.com>
2023-02-27 16:37:35 +0800

    add tab in member and conversion sub menu

--- src/views/system-setting/conversionRecords/index.vue
+++ src/views/system-setting/conversionRecords/index.vue
@@ -1,14 +1,41 @@
 <template>
   <div style="padding: 1rem;">
-    <AGRecord />
-    <OGRecord />
-    <BBRecord />
-    <MGRecord />
-    <PTRecord />
-    <ChessRecord />
+    <el-tabs
+      v-model="activeName"
+      class="parameter-tabs"
+      @tab-click="handleClick"
+    >
+      <el-tab-pane label="AG Conversion Record" name="first">
+        <MemberDeposit />
+        <AGRecord />
+      </el-tab-pane>
+      <el-tab-pane label="OG Conversion Record" name="second">
+        <MemberWithdrawal />
+        <OGRecord />
+      </el-tab-pane>
+      <el-tab-pane label="BB Conversion Record" name="third">
+        <MemberInformation />
+        <BBRecord />
+      </el-tab-pane>
+      <el-tab-pane label="MG Conversion Record" name="fourth">
+        <MemberContactInformation />
+        <MGRecord />
+      </el-tab-pane>
+      <el-tab-pane label="PT Conversion Record" name="fifth">
+        <MemberInformation />
+        <PTRecord />
+      </el-tab-pane>
+      <el-tab-pane label="Chess Conversion Record" name="sixth">
+        <MemberContactInformation />
+        <ChessRecord />
+      </el-tab-pane>
+    </el-tabs>
   </div>
 </template>
-<script>
+
+<script lang="ts" setup>
+import { ref } from 'vue'
+import type { TabsPaneContext } from 'element-plus'
 import AGRecord from '@/views/system-setting/conversionRecords/AGconversionRecord/index.vue'
 import OGRecord from '@/views/system-setting/conversionRecords/OGconversionRecord/index.vue'
 import BBRecord from '@/views/system-setting/conversionRecords/BBconversionRecord/index.vue'
@@ -16,15 +43,17 @@ import MGRecord from '@/views/system-setting/conversionRecords/MGconversionRecor
 import PTRecord from '@/views/system-setting/conversionRecords/PTconversionRecord/index.vue'
 import ChessRecord from '@/views/system-setting/conversionRecords/ChessconversionRecord/index.vue'
 
-export default {
-  components: {
-    AGRecord,
-    OGRecord,
-    BBRecord,
-    MGRecord,
-    PTRecord,
-    ChessRecord,
-  },
+const handleClick = (tab: TabsPaneContext, event: Event) => {
+    console.log(tab, event)
 }
+
+const activeName = ref('first')
 </script>
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.parameter-tabs > .el-tabs_content {
+  padding: 32px;
+  color: #6b778c;
+  font-size: 32px;
+  font-weight: 600;
+}
+</style>
--- src/views/system-setting/member/index.vue
+++ src/views/system-setting/member/index.vue
@@ -1,24 +1,44 @@
 <template>
-  <div style="padding: 1rem;">
-    <MemberDeposit />
-    <MemberWithdrawal />
-    <MemberInformation />
-    <MemberContactInformation />
+  <div style="padding: 0.75rem;">
+    <el-tabs
+      v-model="activeName"
+      class="parameter-tabs"
+      @tab-click="handleClick"
+    >
+      <el-tab-pane label="Member Deposit" name="first">
+        <MemberDeposit />
+      </el-tab-pane>
+      <el-tab-pane label="Member Withdrawal" name="second">
+        <MemberWithdrawal />
+      </el-tab-pane>
+      <el-tab-pane label="Member Information" name="third">
+        <MemberInformation />
+      </el-tab-pane>
+      <el-tab-pane label="Member Contact Information" name="fourth">
+        <MemberContactInformation />
+      </el-tab-pane>
+    </el-tabs>
   </div>
 </template>
-<script>
+<script lang="ts" setup>
+import { ref } from 'vue'
+import type { TabsPaneContext } from 'element-plus'
 import MemberDeposit from '@/views/system-setting/member/memberDeposit/index.vue'
 import MemberWithdrawal from '@/views/system-setting/member/memberWithdrawal/index.vue'
 import MemberInformation from '@/views/system-setting/member/memberInformation/index.vue'
 import MemberContactInformation from '@/views/system-setting/member/memberContactInformation/index.vue'
 
-export default {
-  components: {
-    MemberDeposit,
-    MemberWithdrawal,
-    MemberInformation,
-    MemberContactInformation,
-  },
+const handleClick = (tab: TabsPaneContext, event: Event) => {
+    console.log(tab, event)
 }
+
+const activeName = ref('first')
 </script>
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.parameter-tabs > .el-tabs_content {
+  padding: 32px;
+  color: #6b778c;
+  font-size: 32px;
+  font-weight: 600;
+}
+</style>
