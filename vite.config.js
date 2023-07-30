0f85a4253ec4d877bbd1d27d61dc8e99a7906e6c
scalarvector1003 <happysmile911003@gmail.com>
2023-07-30 15:58:26 +0800

    update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -25,7 +25,8 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import path from 'path'
 import { viteMockServe } from 'vite-plugin-mock'
-import viteSvgIcons from 'vite-plugin-svg-icons'
+// import viteSvgIcons from 'vite-plugin-svg-icons'
+import { createSvgIconsPlugin } from 'vite-plugin-svg-icons'
 import viteESLint from '@ehutch79/vite-eslint'
 
 // https://vitejs.dev/config/
@@ -46,9 +47,11 @@ export default env => {
           setupProdMockServer();
         `,
       }),
-      viteSvgIcons({
+      createSvgIconsPlugin({
         // 指定需要缓存的图标文件夹
-        iconDirs: [path.resolve(__dirname, 'src/assets/svg')],
+        // iconDirs: [path.resolve(__dirname, 'src/assets/svg')],
+        iconDirs: [path.resolve(process.cwd(), 'src/assets/svg')],
+        svgsDir: 'src/svgs',
         // 指定symbolId格式
         symbolId: 'icon-[dir]-[name]',
       }),
