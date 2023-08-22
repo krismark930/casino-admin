bcffb47d349eddb85cc2c7ac842dd0102e968eef
scalarvector1003 <happysmile911003@gmail.com>
2023-08-22 18:36:33 +0800

    test

--- deployments/build.sh
+++ deployments/build.sh
@@ -7,12 +7,12 @@ npm install --legacy-peer-deps
 if [ "${EVN}" = "pre" ];then
     echo "pre"
     cp -f ./.env.pre ./.env.production
-    npm run build
+    npm run build2
 else
     echo "pro"
     # cp -f ./site/${SITE}/.env.production ./.env.production
     # cat ./.env.production
     # cp -f ./site/${SITE}/index.js ./src/router
     # cat ./src/router/index.js
-    npm run build
+    npm run build2
 fi
