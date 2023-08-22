8c01c0d56c9954f9f534f7315519155bbb0458f7
scalarvector1003 <happysmile911003@gmail.com>
2023-08-23 04:26:38 +0800

    AS

--- deployments/build.sh
+++ deployments/build.sh
@@ -9,12 +9,12 @@ npm install
 if [ "${EVN}" = "pre" ];then
     echo "pre"
     cp -f ./.env.pre ./.env.production
-    npm run build2
+    npm run build
 else
     echo "pro"
     # cp -f ./site/${SITE}/.env.production ./.env.production
     # cat ./.env.production
     # cp -f ./site/${SITE}/index.js ./src/router
     # cat ./src/router/index.js
-    npm run build2
+    npm run build
 fi
