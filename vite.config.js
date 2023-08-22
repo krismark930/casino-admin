9b8f1919916ca5387187c7d3c3f98ea8d8885f14
scalarvector1003 <happysmile911003@gmail.com>
2023-08-23 02:37:55 +0800

    test

--- vite.config.js
+++ vite.config.js
@@ -24,10 +24,10 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import path from 'path'
-import { viteMockServe } from 'vite-plugin-mock'
+// import { viteMockServe } from 'vite-plugin-mock'
 // import viteSvgIcons from 'vite-plugin-svg-icons'
 import { createSvgIconsPlugin } from 'vite-plugin-svg-icons'
-import viteESLint from '@ehutch79/vite-eslint'
+// import viteESLint from '@ehutch79/vite-eslint'
 
 // https://vitejs.dev/config/
 export default env => {
@@ -36,17 +36,17 @@ export default env => {
     // base: '/vue3-element-admin-site/',
     plugins: [
       vue(),
-      viteMockServe({
-        ignore: /^_/, // 忽略以下划线`_`开头的文件
-        mockPath: 'mock', // 指定mock目录中的文件全部是mock接口
-        supportTs: false, // mockPath目录中的文件是否支持ts文件，现在我们不使用ts，所以设为false
-        localEnabled: env.mode === 'mock', // 开发环境是否开启mock功能（可以在package.json的启动命令中指定mode为mock）
-        prodEnabled: env.mode === 'mock', // 生产环境是否开启mock功能
-        injectCode: `
-          import { setupProdMockServer } from '../mock/_createProductionServer';
-          setupProdMockServer();
-        `,
-      }),
+      // viteMockServe({
+      //   ignore: /^_/, // 忽略以下划线`_`开头的文件
+      //   mockPath: 'mock', // 指定mock目录中的文件全部是mock接口
+      //   supportTs: false, // mockPath目录中的文件是否支持ts文件，现在我们不使用ts，所以设为false
+      //   localEnabled: env.mode === 'mock', // 开发环境是否开启mock功能（可以在package.json的启动命令中指定mode为mock）
+      //   prodEnabled: env.mode === 'mock', // 生产环境是否开启mock功能
+      //   injectCode: `
+      //     import { setupProdMockServer } from '../mock/_createProductionServer';
+      //     setupProdMockServer();
+      //   `,
+      // }),
       createSvgIconsPlugin({
         // 指定需要缓存的图标文件夹
         // iconDirs: [path.resolve(__dirname, 'src/assets/svg')],
@@ -68,32 +68,32 @@ export default env => {
         '@': path.resolve(__dirname, 'src'),
       },
     },
-    server: {
-      port: 3006,
-      open: true,
-      proxy: {
-        '/api': {
-          target: 'http://dev.api.xxx.com', // 后端接口的域名
-          changeOrigin: true,
-          rewrite: path => path.replace(/^\/api/, ''),
-        },
-      },
-    },
-    optimizeDeps: {
-      include: ['quill']
-    },
-    esbuild: false,
+    // server: {
+    //   port: 3006,
+    //   open: true,
+    //   proxy: {
+    //     '/api': {
+    //       target: 'http://dev.api.xxx.com', // 后端接口的域名
+    //       changeOrigin: true,
+    //       rewrite: path => path.replace(/^\/api/, ''),
+    //     },
+    //   },
+    // },
+    // optimizeDeps: {
+    //   include: ['quill']
+    // },
+    // esbuild: false,
     build: {
-      minify:'terser',
-      terserOptions: {
-        compress: {
-          keep_infinity: true,
-          // 删除console
-          drop_console: true,
-        },
-      },
+      // minify:'terser',
+      // terserOptions: {
+      //   compress: {
+      //     keep_infinity: true,
+      //     // 删除console
+      //     drop_console: true,
+      //   },
+      // },
       // 禁用该功能可能会提高大型项目的构建性能
-      brotliSize: false,
+      // brotliSize: false,
       rollupOptions: {
         external: [             
           //'vue3-quill' // i.e. this syntax does not work, is there something similar that would work?
