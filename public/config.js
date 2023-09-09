6a786188f081a82545c186da7ab5f45340d7c5e0
scalarvector1003 <happysmile911003@gmail.com>
2023-09-09 18:52:43 +0800

    test

--- public/config.js
+++ public/config.js
@@ -1,6 +1,7 @@
-export const VITE_BASE_API = "https://api.pgdemo123.com"
-export const VITE_SOCKET_URL = "https://socket.pgdemo123.com"
-export const VITE_BASE_URL = "https://admin.pgdemo123.com"
+const configStatic={};
+configStatic.VITE_BASE_API = "https://api.pgdemo123.com"
+configStatic.VITE_SOCKET_URL = "https://socket.pgdemo123.com"
+configStatic.VITE_BASE_URL = "https://admin.pgdemo123.com"
 
 // export const VITE_BASE_API = "http://localhost:8000"
 // export const VITE_SOCKET_URL = "http://localhost:3000"
--- src/config/index.js
+++ src/config/index.js
@@ -1,7 +1,7 @@
-import { VITE_BASE_API } from "../../public/config.js";
-import { VITE_SOCKET_URL } from "../../public/config.js";
-import { VITE_BASE_URL } from "../../public/config.js";
+// import { VITE_BASE_API } from "../../public/config.js";
+// import { VITE_SOCKET_URL } from "../../public/config.js";
+// import { VITE_BASE_URL } from "../../public/config.js";
 
-export const BASE_API = VITE_BASE_API;
-export const SOCKET_URL = VITE_SOCKET_URL;
-export const BASE_URL = VITE_BASE_URL;
+export const BASE_API = configStatic.VITE_BASE_API;
+export const SOCKET_URL = configStatic.VITE_SOCKET_URL;
+export const BASE_URL = configStatic.VITE_BASE_URL;
